528619034d01356f8220bb47fdb512ddbd6e1910
Ellie22302 <146311968+Ellie22302@users.noreply.github.com>
2024-03-06 20:58:20 -0500

    First commit

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+var url= 'api.openweathermap.org/data/2.5/forecast?lat=44&lon=44&appid=3e5df3470637ae8ed92dfcd42bd58846'
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=3e5df3470637ae8ed92dfcd42bd58846
+fetch(url)
+
+console.log(url)
