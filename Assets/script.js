0346bf6a3cf118916bb99235e2a4e0f9809ec99c
Ellie22302 <146311968+Ellie22302@users.noreply.github.com>
2024-03-06 22:09:31 -0500

    added a lot

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,41 @@
 var url= 'api.openweathermap.org/data/2.5/forecast?lat=44&lon=44&appid=3e5df3470637ae8ed92dfcd42bd58846'
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=3e5df3470637ae8ed92dfcd42bd58846
-fetch(url)
+const apikey = "3e5df3470637ae8ed92dfcd42bd58846";
+const form = document.querySelector(".form");
+const cityDis = document.querySelector(".search")
+const card = document.querySelector(".card");
 
-console.log(url)
+form.addEventListener("submit",event =>{
+
+        event.preventDefault();
+        const city= cityInput.value;
+
+        if(city){
+
+        }else{
+            disErr("Please Enter City");
+        }
+
+});
+
+async function getWeatherData(city){
+
+}
+
+function disWeather(data){
+
+}
+function emoji(weatherId){
+    
+}
+
+function disErr(message){
+    
+    const errorDis = document.createElement("p")
+    errorDis.textContent = message;
+    errorDis.classList.add("err-dis");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDis)
+}
